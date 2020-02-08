103bfc5591c584fd279011c4f55c8536f2ef45b9
Rhythm-Gupta <rhythm18082@iiitd.ac.in>
2020-02-08 15:03:55 +0530

    Fixed position of p and p2 in matrix

--- Q3.py
+++ Q3.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import copy
 file = open("./Group26/protein.fa", "r")
 #file2=file.read()
 #print(file2)
@@ -32,16 +32,17 @@ print(protein2)
 
 #sumMatrix = np.empty((len(protein1), len(protein2)), dtype = 'S1')
 sumMatrix = []
-for i in range(len(protein1)):
+for i in range(len(protein2)):
 	arr = []
-	for j in range(len(protein2)):
+	for j in range(len(protein1)):
 		arr.append(0)
 	sumMatrix.append(arr)
 
+
 # initialisation
-for i in range(len(protein1)):
-	for j in range(len(protein2)):
-		if (protein1[i]==protein2[j]):
+for i in range(len(protein2)):
+	for j in range(len(protein1)):
+		if (protein2[i]==protein1[j]):
 			sumMatrix[i][j] = 1
 			#print("Value:", sumMatrix[i][j])
 			#print(i," ",j)
@@ -49,40 +50,44 @@ for i in range(len(protein1)):
 			sumMatrix[i][j] = 0
 
 
+
+
 #compute the matrix
 def findMax(a,b):
-	if (a>len(protein1)-1):
-		return len(protein1), len(protein2)
-	if (b>len(protein2)-1):
-		return len(protein1), len(protein2)
+	if (a>len(protein2)-1):
+		return len(protein2), len(protein1)
+	if (b>len(protein1)-1):
+		return len(protein2), len(protein1)
 	else:
 		ansx = a
 		ansy = b
 		ans = 0
 		i = a
 		j = b
-		while(i<len(protein1)):
+		while(j<len(protein1)):
+			if (sumMatrix[i][j]>ans):
+				ans = sumMatrix[i][j]
+				ansx = i
+				ansy = j
+			j+=1
+		i = a
+		j = b
+
+		while(i<len(protein2)):
 			if (sumMatrix[i][j]>ans):
 				ans = sumMatrix[i][j]
 				ansx = i
 				ansy = j
 			
 			i+=1
-		i = a
-		j = b
-		while(j<len(protein2)):
-			if (sumMatrix[i][j]>ans):
-				ans = sumMatrix[i][j]
-				ansx = i
-				ansy = j
-			j+=1
+		
+
 		return ansx, ansy
 
-
-for j in range(len(protein2)-1, -1, -1):
-	for i in range(len(protein1)-1, -1, -1):
+for j in range(len(protein1)-1, -1, -1):
+	for i in range(len(protein2)-1, -1, -1):
 		ax, ay = findMax(i+1, j+1)
-		if (ax<len(protein1) and ay<len(protein2)):
+		if (ax<len(protein2) and ay<len(protein1)):
 			sumMatrix[i][j] += sumMatrix[ax][ay]
 
 out1 = ''
@@ -91,19 +96,19 @@ out2 = ''
 i = -1
 j = -1
 
-while(i<len(protein1) and j<len(protein2)):
+while(i<len(protein2) and j<len(protein1)):
 	ax, ay = findMax(i+1, j+1)
-	if (ax==i+1 and ax<len(protein1) and ay<len(protein2)):
-		#gap in protein1
-		out2 += protein2[j+1:ay+1]
-		l = ay-j
-		out1+=(" "*(l-1) + protein1[ax])
-		
-	elif(ay == j+1 and ax<len(protein1) and ay<len(protein2)):
+	if (ax==i+1 and ax<len(protein2) and ay<len(protein1)):
 		#gap in protein2
-		out1 += protein1[i+1:ax+1]
+		out1 += protein1[j+1:ay+1]
+		l = ay-j
+		out2+=(" "*(l-1) + protein2[ax])
+		
+	elif(ay == j+1 and ax<len(protein2) and ay<len(protein1)):
+		#gap in protein1
+		out2 += protein2[i+1:ax+1]
 		l = ax - i
-		out2+=(" "*(l-1) + protein2[ay])
+		out1+=(" "*(l-1) + protein1[ay])
 	
 	
 	j = ay
